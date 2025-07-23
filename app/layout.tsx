632f14c0b2ea36132d81af0cefff31bc5dc9a5d1
Patel-Priyank-1602 <patelpriyank2526@gmail.com>
2025-07-23 10:42:30 +0530

    Add

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "PyShell | Docs",
+  title: "PyShell | Documentation",
   description: "An advanced Python-based terminal with powerful features for developers",
   icons: {
     icon: "/fav.png",
